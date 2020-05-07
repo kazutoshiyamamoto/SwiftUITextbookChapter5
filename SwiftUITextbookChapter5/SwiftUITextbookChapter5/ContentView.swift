0891943a03a2049f0d8b9e546a3e7dc94522346c
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-05-07 16:09:25 +0900

    ボタンを追加

---
+++ SwiftUITextbookChapter5/SwiftUITextbookChapter5/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITextbookChapter5
+//
+//  Created by home on 2020/05/07.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
