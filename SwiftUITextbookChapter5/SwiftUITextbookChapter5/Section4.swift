ac33bee822e68f15c688480258da0a148f417289
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-05-11 11:24:50 +0900

    Sliderを追加

---
+++ SwiftUITextbookChapter5/SwiftUITextbookChapter5/Section4.swift
@@ -0,0 +1,21 @@
+//
+//  Section4.swift
+//  SwiftUITextbookChapter5
+//
+//  Created by home on 2020/05/11.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct Section4: View {
+    var body: some View {
+        Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Section4_Previews: PreviewProvider {
+    static var previews: some View {
+        Section4()
+    }
+}
