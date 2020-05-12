bd4a9982d3f38c576acd957afdcdf6de7d3e4001
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-05-12 12:30:00 +0900

    選択した色をカラービューとテキストで表示

--- SwiftUITextbookChapter5/SwiftUITextbookChapter5/Section5.swift
+++ SwiftUITextbookChapter5/SwiftUITextbookChapter5/Section5.swift
@@ -10,13 +10,22 @@ import SwiftUI
 
 struct Section5: View {
     @State private var selectColor = 0
+    let colorViews = [Color.red, Color.green, Color.blue]
+    let colorNames = ["Red", "Green", "Blue"]
     
     var body: some View {
         VStack {
             Picker(selection: $selectColor, label: Text("Color")) {
                 Text("Red").tag(0)
                 Text("Green").tag(1)
-                Text("Green").tag(2)
+                Text("Blue").tag(2)
+            }
+            
+            HStack {
+                colorViews[selectColor]
+                    .frame(width: 50.0, height: 100.0)
+                Text("value: \(colorNames[selectColor])")
+                    .frame(width: 70.0)
             }
         }
         .padding()
