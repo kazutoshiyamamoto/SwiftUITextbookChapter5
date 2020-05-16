4295af8803f07a6b14e36fd1976d1fbf578704c5
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-05-16 17:19:09 +0900

    初期表示画面を変更

--- SwiftUITextbookChapter5/SwiftUITextbookChapter5/Section8.swift
+++ SwiftUITextbookChapter5/SwiftUITextbookChapter5/Section8.swift
@@ -26,6 +26,47 @@ struct Section8: View {
             Color.white.onTapGesture {
                 UIApplication.shared.endEditing()
             }
+            
+            VStack (alignment: .leading) {
+                HStack {
+                    Text("個数").padding(.horizontal, 0)
+                    TextField("0", text: $kosu)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .frame(width: 80.0)
+                }
+                .font(.title)
+                .frame(width: 300.0)
+                
+                Group {
+                    if kosuCheck(min: 1, max: 10) {
+                        Text("\(price())円です。")
+                            .font(.title)
+                    } else {
+                        Text("個数は1〜10個を入れてください。")
+                            .foregroundColor(.red)
+                            .font(.headline)
+                    }
+                }
+                .frame(width: 300.0, height: 30.0)
+            }
+            .position(x: 200.0, y: 200.0)
+        }
+    }
+    
+    func kosuCheck(min: Int, max: Int) -> Bool {
+        guard let num = Int(kosu) else {
+            return false
+        }
+        return (min...max).contains(num)
+    }
+    
+    func price() -> Int {
+        if let num = Double(kosu) {
+            let result = Int(tanka * num * tax)
+            return result
+        } else {
+            return -1
         }
     }
 }
